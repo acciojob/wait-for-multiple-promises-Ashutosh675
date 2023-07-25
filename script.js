db36b63fb85b01f1c111320827e6b45f760f6b84
Ashutosh singh <75128637+Ashutosh675@users.noreply.github.com>
2023-07-25 23:57:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+
+function updateElementText() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("Hello, world!");
+    }, 1000);
+  });
+}
+
+// Update HTML element text
+const outputElement = document.getElementById("output");
+updateElementText().then((text) => {
+  outputElement.textContent = text;
+});
